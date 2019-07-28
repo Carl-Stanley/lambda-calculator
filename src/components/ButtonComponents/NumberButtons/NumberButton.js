06fca8216e3590ce08aaab5960ef8dde98d07e41
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-07-28 16:53:36 -0700

    NumberButtons.js and Numbers.js testing. Added functionality. Finally, a breakthrough.

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,12 @@
 import React from "react";
-
-const NumberButton = () => {
+const NumberButton = (props) => {
+  const {numbers} = props;
   return (
     <>
+      <button>{numbers}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default NumberButton;
