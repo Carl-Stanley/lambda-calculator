7aead0f6ad37f25c3a5ec0f61c347100ce7b804c
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-07-28 22:20:21 -0700

    commit

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,12 +1,30 @@
 import React from "react";
 const NumberButton = (props) => {
   const {numbers} = props;
+  
+  
+  
+
+  if((numbers === "0") ) {
+
+  return (
+    <>
+      <button className="zeroB" data-num={numbers}>{numbers}</button>
+      {/* Display a button element rendering the data being passed down from the parent container on props */}
+    </>
+  );
+ } else {
+
   return (
     <>
       <button className="numberB" data-num={numbers}>{numbers}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
+
+
+
+ }
 };
 
 export default NumberButton;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -3,15 +3,24 @@ import React from "react";
 const SpecialButton = (props) => {
 
   const {specials} = props;
-  return (
-    <> 
-    
-      <button className="sb" data-spec={specials}>{specials}</button>
+  
+  
 
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+    return (
+      <> 
+      
+        <button className="sb" data-spec={specials}>{specials}</button>
+  
+        {/* Display a button element rendering the data being passed down from the parent container on props */}
+      
+      </>
     
-    </>
-  );
+    );
+
+
+
+ 
+
 };
 
 export default SpecialButton;
